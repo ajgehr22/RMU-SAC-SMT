3007c07e8765f8c167394c8337541f5a29eb0289
Adam Gehr <ajgehr22@gmail.com>
2024-07-20 15:22:52 -0400

    Update

--- Gehr/First steps.R
+++ Gehr/First steps.R
@@ -24,6 +24,8 @@ SS_ids <- SS_ids[SS_ids < 1000]
 length(SS_ids)
 print(SS_ids)
 
+game_info_SS <- na.omit(game_info_SS)
+
 # collect unique second basemen IDS
 
 game_info_2B <- game_info %>% 
@@ -36,25 +38,46 @@ SecondB_ids <- SecondB_ids[SecondB_ids < 1000]
 length(SecondB_ids)
 print(SecondB_ids)
 
+game_info_2B <- na.omit(game_info_2B)
+
 # filter for plays in 1A first day 1883 where the shortstop is involved ***** PRACTICE
 
-shortstop_involved <- game_events %>% 
+shortstop_involved_practice <- game_events %>% 
   filter(Season == "Season_1883",
          HomeTeam == "Home1A",
          Day == "day_001",
          player_position == 6) %>% 
   collect()
 
-shortstop_involved$player_ID <- 0
+shortstop_involved_practice$player_ID <- 0
+
+# filter for all 1A plays 1883 where the shortstop is involved
+
+shortstop_involved_1A <- game_events %>% 
+  filter(Season == "Season_1883",
+         HomeTeam == "Home1A",
+         player_position == 6) %>% 
+  collect()
+
+shortstop_involved_1A$player_ID <- 0
 
 # add shortstop ID to game info
 
-#*************** DOES NOT WORK ***************#
+for(i in 1:340){
+  for(j in 1:length(game_info_SS$game_str)){
+    if(game_info_SS$game_str[j] == shortstop_involved_1A$game_str[i] & game_info_SS$shortstop[j] < 1000){
+        
+        shortstop_involved_1A$player_ID[i] <- game_info_SS$shortstop[j]
+        
+    }
+  }
+}
 
-for(i in 1:length(shortstop_involved$game_str)){
-  for(j in 1:length(game_info_SS)){
-    if(game_info_SS$game_str[j] == shortstop_involved$game_str[i] & game_info_SS$shortstop[j] < 1000){
-      shortstop_involved$player_ID[i] <- game_info_SS$shortstop[j]
+for(i in 341:length(shortstop_involved_1A$game_str)){
+  for(j in 1:length(game_info_SS$game_str)){
+    if(game_info_SS$game_str[j] == shortstop_involved_1A$game_str[i] & game_info_SS$shortstop[j] < 1000){
+      
+      shortstop_involved_1A$player_ID[i] <- game_info_SS$shortstop[j]
       
     }
   }
