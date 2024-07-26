63200f51a33f255bc69c1514ea781e15b418bedd
Josh Knapp <jxkst464@mail.rmu.edu>
2024-07-26 13:20:16 -0400

    7/26/24 - allowing everyone to input their paths for their starter code.

--- Gehr/Player Position Breakdown.R
+++ Gehr/Player Position Breakdown.R
@@ -2,7 +2,14 @@
 
 ## read in starter code
 
+#Adam
 source("C:\\SMT\\2024_SMT_Data_Challenge\\2024_SMT_Data_Challenge\\SMT_Data_starter.R")
+
+#Josh
+source("~/Desktop/Analytics Competitions:Research/SMT 2024/2024_SMT_Data_Challenge/SMT_Data_starter.R")
+
+#Liam input your source that can load the starter code
+
 
 ## bring in shortstop play IDs and clean up
 
